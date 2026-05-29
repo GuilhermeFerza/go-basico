5d9ae2fe29f69e9281973eb9880f5dce1ab5653c
Guilherme Ferza <guilherme.desouzaferreira2004@gmail.com>
2026-05-29 02:48:27 -0300

    refactor main.go to remove unused byte manipulation code and simplify main function

--- aula/testeComByte/main.go
+++ aula/testeComByte/main.go
@@ -1,29 +1,68 @@
-/*teste sem significado, so tentando entender mais endereco de memoria */
+// // /*teste sem significado, so tentando entender mais endereco de memoria */
+
+// // package main
+
+// // import (
+// // 	"encoding/binary"
+// // 	"fmt"
+// // )
+
+// // func main() {
+
+// // 	b := []byte{0x68, 0x09, 0x00, 0x00}
+
+// // 	novoSlice := make([]byte, 0)
+// // 	num := int(binary.LittleEndian.Uint32(b))
+// // 	num << 1
+// // 	novoNum := int(binary.LittleEndian.Uint32(novoSlice))
+// // 	fmt.Println(num)
+// // 	fmt.Println(novoNum)
+// // 	//          b := []byte{0x68, 0x09, 0x00, 0x00}
+// // 	resultadoNovo := []byte{0xD0, 0x12, 0x00, 0x00}
+
+// // 	resultado := int(binary.LittleEndian.Uint32(resultadoNovo))
+// // 	fmt.Println(resultado)
+// // }
+
+// // //00 00 12 D0
+
+// /*teste sem significado, so tentando entender mais endereco de memoria */
+
+// package main
+
+// import (
+// 	"encoding/binary"
+// 	"fmt"
+// )
+
+// func main() {
+
+// 	b := []byte{0x68, 0x09, 0x00, 0x00}
+
+// 	num := int(binary.LittleEndian.Uint32(b))
+// 	byteShifted := num >> 1
+// 	fmt.Println(byteShifted)
+
+// 	var bytes byte = 1
+
+// 	novoBytes := bytes << 1
+// 	numByte := byte(binary.)
+// 	fmt.Println(novoBytes)
+
+// }
+
+// //00 00 12 D0
 
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
 )
 
 func main() {
 
-	b := []byte{0x68, 0x09, 0x00, 0x00}
+	num := 255
+	alt := fmt.Sprintf("%08b", num)
+	fmt.Println(alt)
 
-	novoSlice := make([]byte, 0)
-	for _, bytes := range b {
-		novoSlice = append(novoSlice, bytes<<1)
-	}
-	num := int(binary.LittleEndian.Uint32(b))
-	novoNum := int(binary.LittleEndian.Uint32(novoSlice))
-	fmt.Println(num)
-	fmt.Println(novoNum)
-	//          b := []byte{0x68, 0x09, 0x00, 0x00}
-	resultadoNovo := []byte{0xD0, 0x12, 0x00, 0x00}
-
-	resultado := int(binary.LittleEndian.Uint32(resultadoNovo))
-	fmt.Println(resultado)
 }
-
-//00 00 12 D0
