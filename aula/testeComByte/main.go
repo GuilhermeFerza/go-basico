965539fa2f0033fb0a7af1a865fd0b2ce26076aa
Guilherme Ferza <guilherme.desouzaferreira2004@gmail.com>
2026-05-29 02:21:40 -0300

    refactor main.go to update byte array and enhance output with new calculations

--- aula/testeComByte/main.go
+++ aula/testeComByte/main.go
@@ -1,3 +1,5 @@
+/*teste sem significado, so tentando entender mais endereco de memoria */
+
 package main
 
 import (
@@ -7,10 +9,21 @@ import (
 
 func main() {
 
-	b := []byte{0x20, 0xA1, 0x07, 0x00}
+	b := []byte{0x68, 0x09, 0x00, 0x00}
 
+	novoSlice := make([]byte, 0)
+	for _, bytes := range b {
+		novoSlice = append(novoSlice, bytes<<1)
+	}
 	num := int(binary.LittleEndian.Uint32(b))
-
+	novoNum := int(binary.LittleEndian.Uint32(novoSlice))
 	fmt.Println(num)
+	fmt.Println(novoNum)
+	//          b := []byte{0x68, 0x09, 0x00, 0x00}
+	resultadoNovo := []byte{0xD0, 0x12, 0x00, 0x00}
 
+	resultado := int(binary.LittleEndian.Uint32(resultadoNovo))
+	fmt.Println(resultado)
 }
+
+//00 00 12 D0
